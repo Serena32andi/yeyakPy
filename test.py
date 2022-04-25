358b165a85fdc273e7f5da0531b6e9a3507cef94
Serena32.andi <dmswls784@gmail.com>
2022-04-26 08:44:18 +0900

    message

---
+++ test.py
@@ -0,0 +1,42 @@
+from xml.dom.minidom import Element
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import winsound
+import time
+import ctypes
+import sys
+
+
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get("https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S220414202040183665&code=T500&dCode=T502&sch_order=1&sch_choose_list=&sch_type=&sch_text=%EB%82%9C%EC%A7%80&sch_recpt_begin_dt=&sch_recpt_end_dt=&sch_use_begin_dt=&sch_use_end_dt=&sch_reqst_value=")
+# driver.get("https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S220414192158211658&code=T500&dCode=T502&sch_order=1&sch_choose_list=&sch_type=&sch_text=&sch_recpt_begin_dt=&sch_recpt_end_dt=&sch_use_begin_dt=&sch_use_end_dt=&sch_reqst_value=")
+
+
+driver.find_element(by=By.CLASS_NAME, value="pop_x").click()
+
+counter = 1
+
+while True:
+
+    element = driver.find_element(by=By.XPATH, value='//*[@id="calendar"]/table/tbody')
+
+    tds = element.find_elements(by=By.TAG_NAME, value="td")
+    for td in tds:
+        nm = td.get_attribute("class")
+        if nm == "able":
+            winsound.Beep(2500, 1000)
+            date = td.find_element(by=By.TAG_NAME, value="span").text
+            ctypes.windll.user32.MessageBoxW(0, "available :" + date, "alert", 1)
+    
+    if counter==10 :
+        print("-")
+        counter = 0
+    else :
+        print(".")
+        counter+=1
+    
+    driver.refresh()
+    time.sleep(10)
+    driver.find_element(by=By.CLASS_NAME, value="pop_x").click()
